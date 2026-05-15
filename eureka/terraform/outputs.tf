5acdeef36acdb1ae9627fc3737afc056af229e97
KS <ks@example.com>
2026-05-15 20:43:05 +0530

    ds

--- eureka/terraform/outputs.tf
+++ eureka/terraform/outputs.tf
@@ -1,3 +1,18 @@
+output "eureka_instance_id" {
+  description = "ID of the Eureka EC2 instance"
+  value       = aws_instance.eureka.id
+}
+
+output "eureka_public_ip" {
+  description = "Public IP of Eureka instance"
+  value       = aws_instance.eureka.public_ip
+}
+
+output "eureka_private_ip" {
+  description = "Private IP of Eureka instance"
+  value       = aws_instance.eureka.private_ip
+}
+
 output "eureka_dashboard_url" {
   description = "URL to access Eureka dashboard"
   value       = "http://${aws_instance.eureka.public_ip}:8761"
