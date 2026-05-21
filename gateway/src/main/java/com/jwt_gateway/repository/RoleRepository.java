02b913f36977d3952e5ad75fa5e752cd71a972b7
KS <ks@example.com>
2026-05-21 19:17:06 +0530

    fff

--- gateway/src/main/java/com/jwt_gateway/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jwt_gateway.repository;
-
-import com.jwt_gateway.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
