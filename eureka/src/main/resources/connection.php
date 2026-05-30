5d5fccf939148b0f5c1139df641b91310fed68c1
kartikeya <64151789+kartikeyasoft@users.noreply.github.com>
2026-05-30 06:50:43 +0530

    Create connection.php

---
+++ eureka/src/main/resources/connection.php
@@ -0,0 +1,5 @@
+echo '<?php' > connection.php
+echo '$db_host = "10.0.0.1";' >> connection.php
+echo '$db_user = "admin";' >> connection.php
+echo '$db_password = "admin123";' >> connection.php
+echo '?>' >> connection.php
